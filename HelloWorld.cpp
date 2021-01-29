844e2dfe2e32f9be2a13311d436983ea4f800c8b
Ragib Miah <ragib@wayne.edu>
2021-01-29 16:55:08 -0500

    merge

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,6 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+//Author: Ragib Miah <ragib@wayne.edu>
+
 // Header file for input output functions 
 #include<iostream>  
   
